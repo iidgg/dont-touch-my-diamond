249f2da80c5f871892897822498a8052983ff101
idrni <idrni@icloud.com>
2023-04-30 05:41:14 +0300

    a window?

--- game.py
+++ game.py
@@ -1,5 +1,16 @@
 import pygame
 
-WIDTH, HEIGHT = 1000, 800
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WIDTH, HEIGHT = 1080, 720
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Cats")
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("purple")
+
+    clock.tick(60)
