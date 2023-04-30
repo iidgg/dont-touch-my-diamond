b06a337eebe19349eb9b389937f02bc5aba2204e
idrni <idrni@icloud.com>
2023-04-30 11:59:04 +0300

    Commenting, too soon to comment but why not. modified:   src/game.py added:   the code looks like shit now

--- src/game.py
+++ src/game.py
@@ -1,29 +1,33 @@
 import pygame
-from movements.player import playerMovments
+from movements.player import playerMovments # Define a function to move the player.
 
 
-pygame.init()
-WIDTH, HEIGHT = 1080, 720
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Don't touch my diamons!")
-clock = pygame.time.Clock()
-running = True
+pygame.init() # Initialize Pygame.
+WIDTH, HEIGHT = 1080, 720 # Define the screen size.
+screen = pygame.display.set_mode((WIDTH, HEIGHT)) # Create the screen.
+pygame.display.set_caption("Don't touch my diamons!") # Set the window title.
+clock = pygame.time.Clock() # Create a clock object.
+running = True # Create a variable to track if the game is running.
 
-playerSkin = pygame.image.load("src/images/woodcutter/index.png")
-playerPOS = oldPlayerPOS = pygame.Vector2(WIDTH / 2, HEIGHT / 2)
-playerSpeed = 3
+playerSkin = pygame.image.load("src/images/woodcutter/index.png") # Load the player image. a.k.a Skin
+playerPOS = oldPlayerPOS = pygame.Vector2(WIDTH / 2, HEIGHT / 2) # Create a variable to store the player's position.
+playerSpeed = 3 # Create a variable to store the player's speed.
 
 while running:
+    # Get all events that happened since the last frame.
     for event in pygame.event.get():
+        # If the user closed the window, quit the game.
         if event.type == pygame.QUIT:
             running = False
-    screen.fill("blue")
-    screen.blit(playerSkin, playerPOS)
+
+    screen.fill("blue") # Fill the screen with blue.
+    screen.blit(playerSkin, playerPOS) # Draw the player on the screen.
 
     oldPlayerPOS = playerPOS
     playerMovments(pygame, playerPOS, playerSpeed)
 
     pygame.display.flip() # Flip to next frame
-    clock.tick(60)
+    clock.tick(60) # Wait for 1/60th of a second.
 
+# Quit Pygame.
 pygame.quit()
