255cb3d1f641d85cf18c268966a8746a40d4d68d
idrni <idrni@icloud.com>
2023-04-30 07:34:37 +0300

    red circle moving

--- game.py
+++ game.py
@@ -7,12 +7,26 @@ pygame.display.set_caption("Cats")
 clock = pygame.time.Clock()
 running = True
 
+playerPOS = pygame.Vector2(WIDTH / 2, HEIGHT / 2)
+playerSpeed = 3
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
     screen.fill("purple")
+    pygame.draw.circle(screen, "red", playerPOS, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        playerPOS.y -= playerSpeed
+    if keys[pygame.K_s]:
+        playerPOS.y += playerSpeed
+    if keys[pygame.K_a]:
+        playerPOS.x -= playerSpeed
+    if keys[pygame.K_d]:
+        playerPOS.x += playerSpeed
+
     pygame.display.flip()
 
     clock.tick(60)
