55c00aa72c4a6335aab3ea90824877eccf34ef68
idrni <idrni@icloud.com>
2023-04-30 11:50:57 +0300

    New player skin modified:   src/game.py new file:   src/images/Woodcutter/index.png deleted:    src/images/player.png

--- src/game.py
+++ src/game.py
@@ -5,12 +5,12 @@ from movements.player import playerMovments
 pygame.init()
 WIDTH, HEIGHT = 1080, 720
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Cats")
+pygame.display.set_caption("Don't touch my diamons!")
 clock = pygame.time.Clock()
 running = True
 
-playerSkin = pygame.image.load("src/images/player.png")
-playerPOS = pygame.Vector2(WIDTH / 2, HEIGHT / 2)
+playerSkin = pygame.image.load("src/images/woodcutter/index.png")
+playerPOS = oldPlayerPOS = pygame.Vector2(WIDTH / 2, HEIGHT / 2)
 playerSpeed = 3
 
 while running:
@@ -20,6 +20,7 @@ while running:
     screen.fill("blue")
     screen.blit(playerSkin, playerPOS)
 
+    oldPlayerPOS = playerPOS
     playerMovments(pygame, playerPOS, playerSpeed)
 
     pygame.display.flip() # Flip to next frame
