a610f88a5925c7c63c40ad6957ad31f3403ecea4
idrni <idrni@icloud.com>
2023-05-01 07:44:59 +0300

    Classes? Fuked Animate but now the player in his own class modified:   src/game.py deleted:    src/utils/movements/player.py modified:   src/utils/player.py

--- src/game.py
+++ src/game.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-from utils.movements.player import playerMovments # Define a function to move the player.
+from utils.player import Player
 
 
 pygame.init() # Initialize Pygame.
@@ -9,13 +9,7 @@ pygame.display.set_caption("Don't touch my diamons!") # Set the window title.
 clock = pygame.time.Clock() # Create a clock object.
 running = True # Create a variable to track if the game is running.
 
-playerSkin = pygame.image.load("src/images/woodcutter/walk.png") # Load the player image. a.k.a Skin
-playerPOS = oldPlayerPOS = pygame.Vector2(WIDTH / 2, HEIGHT / 2) # Create a variable to store the player's position.
-playerSpeed = 0.5 # Create a variable to store the player's speed.
-
-playerWidth = playerSkin.get_width()
-playerHeight = playerSkin.get_height()
-walkingFrame = 0
+player = Player(WIDTH / 2, HEIGHT / 2, 0.5)
 
 while running:
     # Get all events that happened since the last frame.
@@ -25,18 +19,9 @@ while running:
             running = False
 
     screen.fill("blue") # Fill the screen with blue.
-    # screen.blit(playerSkin, playerPOS) # Draw the player on the screen.
-    if walkingFrame % 1 == 0:
-        subsurface = playerSkin.subsurface((walkingFrame * 48, 0, 48, 48))
 
-    screen.blit(subsurface, playerPOS)
-    if walkingFrame >= (6 - 0.1):
-        walkingFrame = 0
-    else:
-        walkingFrame = round(walkingFrame + 0.1, 2)
-    
-    oldPlayerPOS = playerPOS
-    playerMovments(pygame, playerPOS, playerSpeed, WIDTH, HEIGHT, playerWidth, playerHeight)
+    subsd = player.updateAllPlayerIntents()
+    screen.blit(subsd, player.playerPOS)
 
     pygame.display.flip() # Flip to next frame
     clock.tick(60) # Wait for 1/60th of a second.
--- src/utils/player.py
+++ src/utils/player.py
@@ -17,4 +17,48 @@ class Player:
 
         self.walkingFrame = 0
 
-        self.speed = speed
+        self.playerSpeed = speed
+
+    def movePlayer(self):
+        keys = pygame.key.get_pressed()
+    
+        # Check if the player is about to go off screen.
+        if self.playerPOS.x < 0:
+            self.playerPOS.x = 0
+        elif self.playerPOS.x >= WIDTH - self.playerWidth:
+            self.playerPOS.x = WIDTH - self.playerWidth
+        
+        if self.playerPOS.y < 0:
+            self.playerPOS.y = 0
+        elif self.playerPOS.y >= HEIGHT - self.playerHeight:
+            self.playerPOS.y = HEIGHT - self.playerHeight
+    
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.playerPOS.y -= self.playerSpeed
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.playerPOS.y += self.playerSpeed
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.playerPOS.x -= self.playerSpeed
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.playerPOS.x += self.playerSpeed
+        self.oldPlayerPOS = self.playerPOS
+
+    def updateAllPlayerIntents(self):
+        self.movePlayer()
+
+        if self.walkingFrame % 1 == 0:
+            subsurface = self.playerSkin.subsurface((self.walkingFrame * 48, 0, 48, 48))
+
+        return subsurface
+
+    # if walkingFrame % 1 == 0:
+    #     subsurface = playerSkin.subsurface((walkingFrame * 48, 0, 48, 48))
+
+    # screen.blit(subsurface, playerPOS)
+    # if walkingFrame >= (6 - 0.1):
+    #     walkingFrame = 0
+    # else:
+    #     walkingFrame = round(walkingFrame + 0.1, 2)
+    
+    # oldPlayerPOS = playerPOS
+    # playerMovments(pygame, playerPOS, playerSpeed, WIDTH, HEIGHT, playerWidth, playerHeight)
