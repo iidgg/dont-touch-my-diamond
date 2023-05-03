30cc7e9888852576878a45f2db4b348c1355e35d
idrni <idrni@icloud.com>
2023-05-03 07:56:36 +0300

    This commit is fucked up new file:   src/images/Woodcutter/run/left.png new file:   src/images/Woodcutter/run/right.png deleted:    src/images/Woodcutter/walk/left.png deleted:    src/images/Woodcutter/walk/right.png modified:   src/utils/player.py

--- src/utils/player.py
+++ src/utils/player.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 
+
 class Player:
     def __init__(self, x, y, speed):
         self.x = x
@@ -8,7 +9,7 @@ class Player:
 
         self.playerSpeed = speed
 
-        self.playerDirection = self.playerSkin = self.playerWalkingSkin = None # None is NULL
+        self.playerDirection = self.playerSkin = self.playerWalkingSkin = None  # None is NULL
         self.updatePlayerDirection("right")
 
         self.playerWidth = self.playerSkin.get_width()
@@ -16,31 +17,41 @@ class Player:
 
         self.playerSurface = self.playerSkin
 
-        self.playerPOS = self.playerOldPOS = pygame.Vector2(WIDTH / 2, HEIGHT / 2) # Create a variable to store the player's position.
+        # Create a variable to store the player's position.
+        self.playerPOS = self.playerOldPOS = pygame.Vector2(
+            WIDTH / 2, HEIGHT / 2)
 
         self.walkingFrame = 0
+        self.totalWalkingFrames = 8
         self.animationSpeed = 0.1
+
+        self.frameWidth = 128
+        self.frameHeight = 128
+
+        self.totalFramesWidth = self.frameWidth * self.totalWalkingFrames
 
     def updatePlayerDirection(self, direction):
         if direction in ["right", "left"]:
-            self.playerDirection = direction     
-            self.playerSkin = pygame.image.load(f"src/images/woodcutter/index/{self.playerDirection}.png")
-            self.playerWalkingSkin = pygame.image.load(f"src/images/woodcutter/walk/{self.playerDirection}.png")
+            self.playerDirection = direction
+            self.playerSkin = pygame.image.load(
+                f"src/images/woodcutter/index/{self.playerDirection}.png")
+            self.playerWalkingSkin = pygame.image.load(
+                f"src/images/woodcutter/run/{self.playerDirection}.png")
 
     def movePlayer(self):
         keys = pygame.key.get_pressed()
-    
+
         # Check if the player is about to go off screen.
         if self.playerPOS.x < 0:
             self.playerPOS.x = 0
         elif self.playerPOS.x >= WIDTH - self.playerWidth:
             self.playerPOS.x = WIDTH - self.playerWidth
-        
+
         if self.playerPOS.y < 0:
             self.playerPOS.y = 0
         elif self.playerPOS.y >= HEIGHT - self.playerHeight:
             self.playerPOS.y = HEIGHT - self.playerHeight
-    
+
         if keys[pygame.K_UP] or keys[pygame.K_w]:
             self.playerPOS.y -= self.playerSpeed
         if keys[pygame.K_DOWN] or keys[pygame.K_s]:
@@ -56,12 +67,22 @@ class Player:
     def updateAllPlayerIntents(self):
         self.movePlayer()
 
-        if self.walkingFrame % 1 == 0:
-            self.playerSurface = self.playerWalkingSkin.subsurface((self.walkingFrame * 48, 0, 48, 48))
-
-        if self.walkingFrame >= (6 - self.animationSpeed):
+        if self.walkingFrame >= (self.totalWalkingFrames - self.animationSpeed):
             self.walkingFrame = 0
         else:
-            self.walkingFrame = round(self.walkingFrame + self.animationSpeed, 2)
+            self.walkingFrame = round(
+                self.walkingFrame + self.animationSpeed, 2)
+
+        if self.walkingFrame % 1 == 0:
+            if self.playerDirection == "right":
+                self.playerSurface = self.playerWalkingSkin.subsurface(
+                    (self.walkingFrame * 128, 0, 128, 128))
+            else:
+                n = (self.totalWalkingFrames - self.walkingFrame)
+                if n == self.totalWalkingFrames:
+                    n = 0
+
+                self.playerSurface = self.playerWalkingSkin.subsurface(
+                     n * 128, 0, 128, 128)
 
         return self.playerSurface
