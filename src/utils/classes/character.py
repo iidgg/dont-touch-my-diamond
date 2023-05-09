3735d1df1e1bcf8cb8a7090b0c5334494c2db885
idrni <idrni@icloud.com>
2023-05-09 17:35:22 +0300

    Moved/fixed few functions

--- src/utils/classes/character.py
+++ src/utils/classes/character.py
@@ -1,3 +1,5 @@
+import pygame
+import src.constants as C
 from src.utils.classes.object import Object
 
 class Character(Object):
@@ -5,10 +7,54 @@ class Character(Object):
         super().__init__()
     
     def updateAllIntents(self):
-        self.movePlayer()
+        self.updateMovements()
 
-        if not self.position["x"] == self.position["x"] and self.position["y"] == self.position["y"]:
-            if ((self.position["x"] - self.position["x"]) == self.status["walking"]["speed"]) or (abs(self.position["x"] - self.position["x"]) == abs(self.status["walking"]["speed"])):
+        newX, oldX = self.position["x"], self.position["old"]["x"]
+        newY, oldY = self.position["y"], self.position["old"]["y"]
+        isOldXisNewX =  newX == oldX
+        isOldYisNewY = newY == oldY
+        walkingSpeed = self.status["walking"]["speed"]
+
+        if not isOldXisNewX and isOldYisNewY:
+            if ((newX - oldX) == walkingSpeed) or (abs(newX - oldX) == abs(walkingSpeed)):
                 self.animate("walking")
 
         return self.surface
+    
+    def updateMovements(self):
+        keys = pygame.key.get_pressed()
+        self.position["old"]["x"], self.position["old"]["y"] = self.position["x"], self.position["y"]
+
+        # TODO: Bruh bro! the player can go off down screen
+        # Check if the player is about to go off screen.
+        if self.position["x"] < 0:
+            self.position["x"] = 0
+        elif self.position["x"] >= C.screen["width"] - self.status["walking"]["dimensions"]["width"]:
+            self.position["x"] = C.screen["width"] - self.status["walking"]["dimensions"]["width"]
+            
+        if self.position["y"] < 0:
+            self.position["y"] = 0
+        elif self.position["y"] >= C.screen["width"] - self.status["walking"]["dimensions"]["height"]:
+            self.position["y"] = C.screen["width"] - self.status["walking"]["dimensions"]["height"]
+        # TODO: Ends the last todo, the code down there doesn't have anything to do with this bug
+
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.position["y"] -= self.status["walking"]["speed"]
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.position["y"] += self.status["walking"]["speed"]
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.position["x"] -= self.status["walking"]["speed"]
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.position["x"] += self.status["walking"]["speed"]
+
+        if not (self.position["old"]["x"] == self.position["x"] and self.position["old"]["y"] == self.position["y"]):
+            if not self.position["old"]["y"] == self.position["y"]:
+                # Moved up or down
+                ""
+            else:
+                if self.position["x"] - 0.5 == self.position["old"]["x"]:
+                    "Moved right"
+                    self.updateDirection("right")
+                elif self.position["x"] + 0.5 == self.position["old"]["x"]:
+                    "Moved left"
+                    self.updateDirection("left")
