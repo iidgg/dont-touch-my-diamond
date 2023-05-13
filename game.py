ce642c6fba16c302ba38fb402351f037f77837cf
idrni <idrni@icloud.com>
2023-05-13 16:39:45 +0300

    DIAMONDS! don't run this

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 import src.constants as C
 from src.utils.player import Player
+from src.utils.diamonds import Diamonds
 
 
 pygame.init() # Initialize Pygame.
@@ -18,12 +19,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    
     game_canvas = pygame.Surface((C.canvas["width"],C.canvas["height"]))
     game_canvas.fill("blue")
     grass_img = pygame.image.load("src/images/map/grass.png")
     game_canvas.blit(grass_img, (0,0))
+
+    diamonds = Diamonds()
+    diamonds.spawnRandom(game_canvas, 5)
+
     player.render(game_canvas, True)
+
 
     screen.blit(pygame.transform.scale(game_canvas,(C.screen["width"], C.screen["height"])), (0,0))
 
--- src/utils/diamonds.py
+++ src/utils/diamonds.py
@@ -1,6 +1,20 @@
 import pygame
+import random
 from src.utils.classes.object import Object
 
 class Diamonds(Object):
     def __init__(self):
-        Object.__init__()
+        Object.__init__(self)
+
+        self.skin = pygame.transform.scale(pygame.image.load("src/images/objects/rewards/diamond.png"), (11.54, 12.5))
+
+        self.allDiamonds = []
+
+    def spawnRandom(self, screen, count):
+        for i in range(count):
+            x = random.randint(0, screen.get_width())
+            y = random.randint(0, screen.get_height())
+            diamond = pygame.Rect(x, y, self.skin.get_width(), self.skin.get_height())
+
+            screen.blit(self.skin, diamond)
+            self.allDiamonds.append(diamond)
