d0995a344322de0cb321a777a52ef649e1ccef71
idrni <idrni@icloud.com>
2023-05-15 03:15:43 +0300

    Small change, big in future

---
+++ src/utils/classes/skin.py
@@ -0,0 +1,22 @@
+import pygame
+from src.utils.classes.movements import Movements
+
+class Skin(Movements):
+    def __init__(self, isAnimated):
+        # All available animations
+        self.animations = a = ["walking"]
+
+        """"
+        Structure:
+        "Animation"
+        "AnimationSpeed"
+        "Speed * Number = the player speed *Speed = The given speed to the class*"
+        """  # With spaces between each
+        self.AI = [f"{a[0]} 0.1 0.5"]
+        # ^ Animation information
+
+        self.hasSkin = True
+        self.skin = "" # Pygame load something
+        self.skinName = "whitish" # TODO: Remove hardcode
+        
+        self.updateSkins()
