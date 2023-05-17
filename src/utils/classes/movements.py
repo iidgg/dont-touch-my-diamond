22c045a22f54decced363526eb726a15bf780fc6
idrni <idrni@icloud.com>
2023-05-17 04:01:48 +0300

    Move cute function

--- src/utils/classes/movements.py
+++ src/utils/classes/movements.py
@@ -1,3 +1,6 @@
+import pygame
+import src.constants as C
+
 class Movements():
     def __init__(self):
         self.isMoveable = True
@@ -13,3 +16,36 @@ class Movements():
             self.oldDirection = self.direction
             self.direction = direction
             self.updateSkins()
+    
+    def updateMovements(self):
+        keys = pygame.key.get_pressed()
+        # self.pos["ox"], self.pos["oy"] = self.pos["x"], self.pos["y"]
+        self.swapPosition()
+
+        walkingWidth = self.status["walking"]["dimensions"]["width"]
+        walkingHeight = self.status["walking"]["dimensions"]["height"]
+
+        # TODO: Bruh bro! the player CAN go off down screen
+        # Check if the player is about to go off screen.
+        if self.pos["x"] < 0:
+            self.setX(0)
+        elif self.pos["x"] >= C.screen["width"] - walkingWidth:
+            self.setX(C.screen["width"] - walkingWidth)
+            
+        if self.pos["y"] < 0:
+            self.setY(0)
+        elif self.pos["y"] >= C.screen["width"] - walkingHeight:
+            self.setY(C.screen["width"] - walkingHeight)
+        # TODO: Ends the last todo, the code down there doesn't have anything to do with this bug
+
+        ws = self.status["walking"]["speed"] # Walking speed
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.changeYby(-ws)
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.changeYby(ws)
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.changeXby(-ws)
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.changeXby(ws)
+
+        self.followMovements()
