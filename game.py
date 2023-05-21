0e7bc5fea05da868be44a63f68e737c240fb6498
idrni <idrni@icloud.com>
2023-05-21 10:01:09 +0300

    okay, grass is back.. We're recovering

--- game.py
+++ game.py
@@ -11,6 +11,11 @@ running = True # Create a variable to track if the game is running.
 
 player = Player(C.screen["width"] / 2, C.screen["height"] / 2, 0.5)
 
+game_canvas = pygame.Surface((C.canvas["width"],C.canvas["height"]))
+game_canvas.fill("blue")
+
+grass_img = pygame.image.load("src/images/map/grass.png")
+game_canvas.blit(grass_img, (0,0))
 
 while running:
     # Get all events that happened since the last frame.
@@ -19,15 +24,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # game_canvas = pygame.Surface((C.canvas["width"],C.canvas["height"]))
-    # game_canvas.fill("blue")
-    # grass_img = pygame.image.load("src/images/map/grass.png")
-    # game_canvas.blit(grass_img, (0,0))
 
     # player.renderPlayer(game_canvas)
 
-
-    # screen.blit(pygame.transform.scale(game_canvas,(C.screen["width"], C.screen["height"])), (0,0))
+    screen.blit(pygame.transform.scale(game_canvas,(C.screen["width"], C.screen["height"])), (0,0))
 
     pygame.display.flip() # Flip to next frame
     clock.tick(60) # Wait for 1/60th of a second.
