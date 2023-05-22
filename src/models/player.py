5c9f3545ced779ece36dd95daf11969c807525bb
idrni <idrni@icloud.com>
2023-05-22 03:30:43 +0300

    Stable again...

---
+++ src/models/player.py
@@ -0,0 +1,8 @@
+import pygame
+import src.utils.constants as C
+from src.models.character import Character
+
+class Player(Character):
+    def __init__(self, x, y, speed):
+        # Initialize the extended classes
+        Character.__init__(self, speed)
--- src/theGame.py
+++ src/theGame.py
@@ -2,8 +2,38 @@ import pygame
 import src.utils.constants as C
 from src.models.character import Character
 from src.models.diamonds import Diamonds
+from src.models.player import Player
 
-class Player(Character):
-    def __init__(self, x, y, speed):
-        # Initialize the extended classes
-        Character.__init__(self, speed)
+class TheGame():
+    def __init__(self):
+        pygame.init() # Initialize Pygame.
+
+        self.screen = pygame.display.set_mode((C.screen["width"], C.screen["height"])) # Create the screen.
+        pygame.display.set_caption("Don't touch my diamonds!") # Set the window title.
+
+        self.clock = pygame.time.Clock() # Create a clock object.
+        self.running = True # Create a variable to track if the game is running.
+
+        self.player = Player(C.screen["width"] / 2, C.screen["height"] / 2, 0.5)
+
+        self.gameCanvas = pygame.Surface((C.canvas["width"],C.canvas["height"]))
+        self.backgroundImg = pygame.image.load("src/images/map/grass.png")
+
+    def initializeNewFrame(self):
+        self.gameCanvas.blit(self.backgroundImg, (0,0))
+
+    def loadFrame(self):
+        # frameScaled = 
+        self.screen.blit(pygame.transform.scale(self.gameCanvas,(C.screen["width"], C.screen["height"])), (0,0))
+
+        pygame.display.flip() # Flip to next frame
+        self.clock.tick(60) # Wait for 1/60th of a second.
+
+    def updateFrame(self):
+        self.initializeNewFrame()
+
+        self.player.movingHandler()
+        self.player.render(self.gameCanvas)
+
+    def stop(self):
+        self.running = False
